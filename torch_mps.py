9fab2972bc71a8d6b6cc9be2269280a814d7a25e
Kit Burgess <chrisb4130@gmail.com>
2023-12-13 18:42:50 +0000

    Check

---
+++ torch_mps.py
@@ -0,0 +1,19 @@
+import torch
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print (x)
+else:
+    print ("MPS device not found.")
+
+
+# Check that MPS is available
+if not torch.backends.mps.is_available():
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+else:
+    print("MPS Available")
